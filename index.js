48df64f33fe3ee8953c68f9935a7c1a71a059942
Chris <chrislopes1403@comcast.net>
2020-10-06 19:36:52 -0400

    client update 3

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-//test
-//test
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+var router = express.Router();
+
+
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+  
+  }
+
+
+  app.post('/api/login',  async (req,res)=>{
+    res.status(200).send({ message: 'success!'});
+  })
+
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`)
+});
